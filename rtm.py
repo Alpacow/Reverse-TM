3ba29bd16ab47a1332d9a482934266f7723475e0
Francielle <fvpereira@inf.ufsm.br>
2021-01-06 15:52:36 -0300

    lendo arquivo txt de entrada

---
+++ rtm.py
@@ -0,0 +1,32 @@
+class Rtm:
+    nStates = 0
+    nInput = 0
+    nTape = 0
+    nTransitions = 0
+    states = []
+    alphaInput = []
+    alphaTape = []
+    transitions = []
+    input = []
+
+    def __init__(self, nStates, nInput, nTape, nTransitions, states, alphaInput, alphaTape, transitions, _input):
+        self.nStates = nStates
+        self.nInput = nInput
+        self.nTape = nTape
+        self.nTransitions = nTransitions
+        self.states = states
+        self.alphaInput = alphaInput
+        self.alphaTape = alphaTape
+        self.transitions = transitions
+        self.input = _input
+
+    def printValues(self):
+        print("Número de estados: ", self.nStates)
+        print("Estados: ", self.states)
+        print("Número de símbolos do alfabeto de entrada: ", self.nInput)
+        print("Alfabeto de entrada:", self.alphaInput)
+        print("Número de símbolos do alfabeto da fita: ", self.nTape)
+        print("Alfabeto da fita: ", self.alphaTape)
+        print("Número de transições: ", self.nTransitions)
+        print("Transições: ", self.transitions)
+        print("Entrada: ", self.input)

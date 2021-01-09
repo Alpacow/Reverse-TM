882cca8a4debb0121a25b61c64314767da6a20ae
BrunoGottlieb <48501861+BrunoGottlieb@users.noreply.github.com>
2021-01-09 17:32:59 -0300

    09-01-21 - All checks

--- rtm.py
+++ rtm.py
@@ -30,13 +30,33 @@ class Rtm:
         print("Número de transições: ", self.nTransitions)
         print("Transições: ", self.transitions)
         print("Entrada: ", self.input)
-        print("Entrada é válida: ", self.CheckInput())
+        print("Entrada é válida: ", self.checkInput())
+        print("State check: ", self.checkStates())
+        print("Symbols check: ", self.checkSymbols())
+        print("Tape symbols check: ", self.checkTapeSymbols())
+        print("Transitions check: ", self.transitionsCheck())
 
-    def CheckInput(self):
+    def checkInput(self):
         for val in self.input:
             if val not in self.alphaInput:
                 return False
         return True
+
+    def checkStates(self):
+        size = len(self.states.replace(" ", ""))
+        return size == self.nStates
+
+    def checkSymbols(self):
+        size = len(self.alphaInput.replace(" ", ""))
+        return size == self.nInput
+
+    def checkTapeSymbols(self):
+        size = len(self.alphaTape.replace(" ", ""))
+        return size == self.nTape
+
+    def transitionsCheck(self):
+        return self.nTransitions == len(self.transitions)
+
 
         
 

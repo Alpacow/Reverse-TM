91cf851a7655c0d0010e523be3436801f88becf9
BrunoGottlieb <48501861+BrunoGottlieb@users.noreply.github.com>
2021-01-10 00:57:26 -0300

    10-01-21 - First Stage

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ def readFile(file):
     n = [dt[0][i] for i in range(len(dt[0])) if dt[0][i] != ' ']
     rtm = Rtm( int(n[0]), int(n[1]), int(n[2]), int(n[3]), dt[1], dt[2], dt[3], dt[4:-1], dt[len(dt) - 1])
     rtm.printValues()
+    rtm.execution()
 
 def main (args):
     print("*-- Máquina de Turing Reversível --*\n")
 
     readFile(sys.stdin.read())
+
 
 if __name__ == "__main__":sys.exit(main(sys.argv))
--- rtm.py
+++ rtm.py
@@ -9,6 +9,10 @@ class Rtm:
     transitions = []
     input = []
 
+    inputTape = ["B","B"]
+    historyTape = ["B","B"]
+    outputTape = ["B","B"]
+
     def __init__(self, nStates, nInput, nTape, nTransitions, states, alphaInput, alphaTape, transitions, _input):
         self.nStates = nStates
         self.nInput = nInput
@@ -30,11 +34,48 @@ class Rtm:
         print("Número de transições: ", self.nTransitions)
         print("Transições: ", self.transitions)
         print("Entrada: ", self.input)
-        print("Entrada é válida: ", self.checkInput())
+        print("\nInput check: ", self.checkInput())
         print("State check: ", self.checkStates())
         print("Symbols check: ", self.checkSymbols())
         print("Tape symbols check: ", self.checkTapeSymbols())
         print("Transitions check: ", self.transitionsCheck())
+
+    def initTransitions(self): # remove as virgulas e parenteses
+        for i in range(len(self.transitions)):
+            self.transitions[i] = self.transitions[i].replace(",", "")
+            self.transitions[i] = self.transitions[i].replace("(", "")
+            self.transitions[i] = self.transitions[i].replace(")", "")
+
+    def execution(self):
+
+        self.initTransitions()
+
+        print("\n")
+
+        index = 1 # posicao onde se encontra a cabeca na transicao
+        head = 0 # posicao onde se encontra a cabeca na fita
+        offset = 5 # deslocamento ate o outro lado
+        for transition in self.transitions: # loop pelas transicoes
+
+            print("Transition: ", transition) # debug
+            print("Head: ", transition[index]) # debug
+
+            if transition[index] == self.input[head]: # input bate com a condicao da transicao
+                self.inputTape[head] = transition[index + offset] # recebe o simbolo do lado direito da transicao
+                self.historyTape[head] = transition[0] # numero do estagio
+
+            elif transition[index] == "/": # caso o simbolo seja uma barra
+                if transition[index + offset] == "L": # avancar para a direita
+                    print("Left shift") # debug
+                    head -= 1
+                elif transition[index + offset] == "R": # avancar para a esquerda
+                    print("Right shift") # debug
+                    head += 1
+
+        print("\n")
+        print("inputTape: ", self.inputTape)
+        print("historyTape: ", self.historyTape)
+
 
     def checkInput(self):
         for val in self.input:
@@ -56,7 +97,3 @@ class Rtm:
 
     def transitionsCheck(self):
         return self.nTransitions == len(self.transitions)
-
-
-        
-

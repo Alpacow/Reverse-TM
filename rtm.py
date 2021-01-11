9c8c34fe60d68d922586939f4ffe8edff39723c4
Francielle <fvpereira@inf.ufsm.br>
2021-01-11 12:41:03 -0300

    agr so falta funciona lixo

--- rtm.py
+++ rtm.py
@@ -28,8 +28,8 @@ class Rtm:
         self.input = _input
 
         self.inputTape = [i for i in self.input]
-        #self.historyTape = ["B" for i in range(len(self.input))]
-        self.outputTape = ["B" for i in range(len(self.input))]
+        self.inputTape.append("B")
+        self.outputTape = ["B" for i in range(len(self.input) + 1)]
 
     def printValues(self):
         print("Número de estados: ", self.nStates)
@@ -65,24 +65,27 @@ class Rtm:
             _next = index + 1
             newT.append(str(index) + ',' + check + '/B=' + str(_next) + ',' + symbol + sigma + 'B')
             next2 = _next + 1
-            newT.append(str(_next) + ',' + '/B/=' + str(next2) + ',' + sigma + str(_next) + '0')
+            newT.append(str(_next) + ',' + '/B/=' + str(next2) + ',' + sigma + sigma + '0')
             index += 2
             self.nTransitions = next2
         return newT
 
     def execution(self):
-        print("Estagio 1:\n")
+        print("Estado inicial:")
+        self.printTapes()
+        print("Estagio 1:")
         self.Stage_1()
         self.printTapes()
         print("Estagio 2:")
         self.Stage_2()
-        for t in self.transitions:
-            print(t)
-        #self.Stage_3()
+        self.printTapes()
+        self.Stage_3()
+        self.printTapes()
 
     def Stage_1(self):
         self.initTransitions()
         self.transitions = self.getQuadruples()
+        #self.historyTape = ["B" for i in range(len(self.input))]
         #print("Transitions: ", self.transitions)
         for transition in self.transitions: # loop pelas transicoes
             aux = transition.partition("=")
@@ -91,33 +94,11 @@ class Rtm:
             check = left.partition(",")[2][0] # se a entrada for quintupla
             stage = left.partition(",")[0]
             move_write = right.partition(",")[2][0]
-            #check = left[1] # se a entrada for quadrupla
-            #stage = left[0]
-            #move_write = right[1]
-
-            if check != "/":
+            if check != "/": # se nao é uma barra -> ESCREVE
                 self.inputTape[self.head] = move_write # recebe o simbolo do lado direito da transicao
                 self.historyTape.append(stage) # numero do estagio
-                """
-                if move_write == "R" or move_write == "X":
-                    self.headH += 1
-                    # se a cabeça da fita é igual ao tamanho da fita, a fita é aumentada com um "B"
-                    if self.headH == len(self.historyTape):
-                        self.historyTape.append("B")
-                if move_write == "L" or move_write != "X":
-                    if self.headH == len(self.historyTape)-1 or self.historyTape[self.headH] == "B":
-                        self.historyTape = self.historyTape[:self.headH-1]
-                        self.historyTape.append("B")
-                    self.headH -= 1
-                """
-
-            elif check == "/": # caso o simbolo seja uma barra
+            elif check == "/": # caso o simbolo seja uma barra -> MOVE
                 if move_write == "L": # avancar para a esquerda
-                    # se a cabeça da fita é igual ao tamanho da fita -1, ou o cabeça da fita for símbolo branco, diminui a fita
-                    if self.head == len(self.inputTape)-1 or self.inputTape[self.head] == "B":
-                        #print("Diminuindo tamanho da fita:", head,len(self.inputTape)-1, self.inputTape[head]) #debug
-                        self.inputTape = self.inputTape[:self.head-1]
-                        self.inputTape.append("B")
                     self.head -= 1
                 elif move_write == "R": # avancar para a direita
                     self.head += 1
@@ -129,16 +110,44 @@ class Rtm:
         self.invertTransitions()
         self.outputTape = self.inputTape; # copia a fita 1 para a fita 3
 
-    #def Stage_3(self):
+    def Stage_3(self):
+        print("Cabeça fita1", self.head)
+        for transition in self.transitions: # loop pelas transicoes
+            aux = transition.partition("=")
+            left = aux[0]
+            right = aux[2]
+            check = left.partition(",")[2][0] # se a entrada for quintupla
+            move_write = right.partition(",")[2][0]
+            print("Transicao:", transition)
+            if check != "/": # se nao é uma barra -> ESCREVE
+                self.inputTape[self.head] = check # recebe o simbolo do lado direito da transicao
+                self.historyTape = self.historyTape[:-1] # remove o ultimo estagio
+            elif check == "/": # caso o simbolo seja uma barra -> MOVE
+                if move_write == "L": # avancar para a esquerda
+                    self.head -= 1
+                    print("Movendo pra esquerda", self.head)
+                elif move_write == "R": # avancar para a direita
+                    self.head += 1
+                    # se a cabeça da fita é igual ao tamanho da fita, a fita é aumentada com um "B"
+                    if self.head == len(self.inputTape):
+                        self.inputTape.append("B")
+                        print("Movendo pra direita", self.head)
+            print("Fita 1:", self.inputTape)
 
     def invertTransitions(self): # Stage 2
         for index in range(0, len(self.transitions)): # inverte as letras direcionais
-            self.transitions[index] = self.transitions[index].replace("R", "L")
-            self.transitions[index] = self.transitions[index].replace("L", "R")
-            aux = self.transitions[index].partition("=") # inverte a ordem
-            left = aux[0]
-            right = aux[2]
-            self.transitions[index] = right + "=" + left
+            if "R" in self.transitions[index]:
+                self.transitions[index] = self.transitions[index].replace("R", "L")
+            elif "L" in self.transitions[index]:
+                self.transitions[index] = self.transitions[index].replace("L", "R")
+            part = self.transitions[index].partition("=") # inverte a ordem
+            auxLeft = part[0]
+            auxRight = part[2]
+            l = auxLeft.partition(",")
+            r = auxRight.partition(",")
+            newL = r[0] + "," + l[2][0] + r[2][1] + l[2][2]
+            newR = l[0] + "," + r[2][0] + l[2][1] + r[2][2]
+            self.transitions[index] = newL + "=" + newR
         self.transitions.reverse()
 
     def checkInput(self):
